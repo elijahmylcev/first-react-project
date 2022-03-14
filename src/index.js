ac2a347c833efe1bdbe8b1e583e1c3e8cf50dc37
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-14 17:31:36 +0300

    re

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+const element = <h2>Hello world</h2>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  element,
+  document.getElementById('root'),
 );
