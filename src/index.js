f9a160ef14aaa26b7db3057d2d4b0d0aa7869467
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-16 17:15:05 +0300

    Rules for jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
 
-const element = <h2>Hello World</h2>;
+const text = 'Hello world!';
+
+const element = (
+  <div>
+    <h2 className="text">{text}</h2>
+    <input type="text" />
+    <button type="submit">Hello</button>
+    {/* <label htmlFor="" /> */}
+  </div>
+);
 
 // const element = React.createElement('h2', { className: 'greetings' }, 'Hello world!!!');
 
