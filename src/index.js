e052ae9f7830b415bffc1ee49180384e1e334b1e
Elijah <elijahmylcev@gmail.com>
2022-03-16 16:56:10 +0300

    JSX vs native method

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
 
-// const element = <h2>Hello World</h2>;
+const element = <h2>Hello World</h2>;
 
-const element = React.createElement('h2', { className: 'greetings' }, 'Hello world!!!');
+// const element = React.createElement('h2', { className: 'greetings' }, 'Hello world!!!');
 
 ReactDOM.render(
   // <React.StrictMode>
