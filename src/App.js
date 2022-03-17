2d8d0d499b12f9bf310bd8e44f23fd9e7412376b
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-17 13:46:27 +0300

    working with props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 import './App.css';
+import PropTypes from 'prop-types';
+
+// Старый метод
+// class Field extends Component {
+//   render() {
+//     const placeHolder = 'Type here';
+//     const styledField = {
+//       width: '300px',
+//     };
+//     return (
+//       <input
+//         type="text"
+//         placeholder={placeHolder}
+//         style={styledField}
+//       />
+//     );
+//   }
+// }
+
+// Props === {}
 // import { Component } from 'react';
 // Можно использовать стрелочные функции для создания компонентов
 function Header() {
@@ -33,25 +53,27 @@ function App() {
       <Header />
       <Field />
       <Btn />
+      <WhoAmI name="John" surname="Smith" link="http://facebook.com" />
     </div>
   );
 }
-
-// Старый метод
-// class Field extends Component {
-//   render() {
-//     const placeHolder = 'Type here';
-//     const styledField = {
-//       width: '300px',
-//     };
-//     return (
-//       <input
-//         type="text"
-//         placeholder={placeHolder}
-//         style={styledField}
-//       />
-//     );
-//   }
-// }
+function WhoAmI({ name, surname, link }) {
+  return (
+    <div>
+      <h1>
+        My name is
+        {name}
+        , surname -
+        {surname}
+      </h1>
+      <a href={link}>My profile</a>
+    </div>
+  );
+}
+WhoAmI.propTypes = {
+  name: PropTypes.string.isRequired,
+  surname: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
 
 export default App;
