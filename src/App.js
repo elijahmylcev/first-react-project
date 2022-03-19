cf05b3f3a195bc14f817a78968838cac47871d09
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-19 14:21:08 +0300

    classes and state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,91 @@
-import './App.css';
 import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+// function Header() {
+//   return <h2>Hello world!</h2>;
+// }
+
+// function Field() {
+//   const placeHolder = 'Type here';
+//   const styledField = {
+//     width: '300px',
+//   };
+//   return (
+//     <input
+//       type="text"
+//       placeholder={placeHolder}
+//       style={styledField}
+//     />
+//   );
+// }
+
+// function Btn() {
+//   const text = 'Log in';
+//   // const res = () => 'Log in';
+//   const logged = false;
+//   // return <button type="submit">{res()}</button>;
+//   return <button type="submit">{ logged ? 'Enter' : text }</button>;
+// }
+
+class WhoAmI extends Component {
+  constructor() {
+    super();
+    this.state = {
+      years: 1,
+    };
+  }
+
+  increment = () => {
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  render() {
+    const { ...props } = this.props;
+    const { ...state } = this.state;
+    return (
+      <div>
+        <h1>
+          <button onClick={this.increment} type="submit">Increment</button>
+          My name is
+          { props.name}
+          , surname -
+          { props.surname}
+          ,
+          age -
+          {state.years}
+        </h1>
+        <a href={props.link}>My profile</a>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.person = {
+      name: 'John',
+      surname: 'Smith',
+      link: 'http://facebook.com',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <WhoAmI name={this.person.name} surname={this.person.surname} link={this.person.link} />
+      </div>
+    );
+  }
+}
+
+WhoAmI.propTypes = {
+  name: PropTypes.string.isRequired,
+  surname: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
 
 // Старый метод
 // class Field extends Component {
@@ -19,61 +105,6 @@ import PropTypes from 'prop-types';
 // }
 
 // Props === {}
-// import { Component } from 'react';
 // Можно использовать стрелочные функции для создания компонентов
-function Header() {
-  return <h2>Hello world!</h2>;
-}
-
-function Field() {
-  const placeHolder = 'Type here';
-  const styledField = {
-    width: '300px',
-  };
-  return (
-    <input
-      type="text"
-      placeholder={placeHolder}
-      style={styledField}
-    />
-  );
-}
-
-function Btn() {
-  const text = 'Log in';
-  // const res = () => 'Log in';
-  const logged = false;
-  // return <button type="submit">{res()}</button>;
-  return <button type="submit">{ logged ? 'Enter' : text }</button>;
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Field />
-      <Btn />
-      <WhoAmI name="John" surname="Smith" link="http://facebook.com" />
-    </div>
-  );
-}
-function WhoAmI({ name, surname, link }) {
-  return (
-    <div>
-      <h1>
-        My name is
-        {name}
-        , surname -
-        {surname}
-      </h1>
-      <a href={link}>My profile</a>
-    </div>
-  );
-}
-WhoAmI.propTypes = {
-  name: PropTypes.string.isRequired,
-  surname: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-// const text = 'Hello world!';
-
-// const element = (
-//   <div>
-//     <h2 className="text">{text}</h2>
-//     <input type="text" />
-//     <button type="submit">Hello</button>
-//     {/* <label htmlFor="" /> */}
-//   </div>
-// );
-
 // const element = React.createElement('h2', { className: 'greetings' }, 'Hello world!!!');
 
 ReactDOM.render(
